1a2bde387d7e2c833d16777702e5c5a61795d2ff
lookwendyican <wendyalers@gmail.com>
2020-02-06 14:59:56 -0500

    initial files... added reactstrap, bootstrap, redux and react-redux

---
+++ songs/src/Components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+
+//import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Container className="mt-5">
+        <Row>
+          <Col>My boys</Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default App;
---
+++ songs/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./Components/App";
+//THIS IS IMPORTANT FOR BOOTSTRAP TO WORK ON AL COMPONENTS
+import "bootstrap/dist/css/bootstrap.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
