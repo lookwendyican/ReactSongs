50a9ba944d99d5d5ebb6b24488a1fc833bffab52
lookwendyican <wendyalers@gmail.com>
2020-02-08 17:14:53 -0500

    added the SongDetails component wrapped around the connect call

--- songs/src/Components/App.js
+++ songs/src/Components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
 import SongList from "./SongList";
+import SongDetail from "./SongDetails";
 
 const App = () => {
   return (
@@ -8,8 +9,12 @@ const App = () => {
       <Container className="mt-5">
         <Row>
           <Col md={6}>
-            <h1 className="text-center text-info">Select a Song!</h1>
+            <h1 className="text-center text-info">Select a Song</h1>
             <SongList />
+          </Col>
+          <Col md={6}>
+            <h1 className="text-center text-primary"> Song Detail</h1>
+            <SongDetail />
           </Col>
         </Row>
       </Container>
--- songs/src/Components/SongDetails.js
+++ songs/src/Components/SongDetails.js
@@ -1,14 +1,33 @@
-import React, { Component } from 'react',
-import { connect} from "react-redux"
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-const SongDetail = ()=> {
-    return (<div> 
-        Song Detail
-    </div>)
-}
+const SongDetail = ({ song }) => {
+  if (!song) {
+    return (
+      <div>
+        <h5 className="text-center text-danger"> Please select a song.</h5>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h3 className="text-primary"> Details for: </h3>
+      <br />
+      <p>
+        <span className="text-primary">Title:</span> {song.title}
+        <br />
+        <br />
+        <span className="text-primary">Singer: </span>
+        {song.singer}
+        <br />
+        <br />
+        <span className="text-primary">Duration:</span> {song.duration}
+      </p>
+    </div>
+  );
+};
 
-
-const mapStateToProps = (state)=> {
-    
-}
-export default SongDetail;
+const mapStateToProps = state => {
+  return { song: state.selectedSong };
+};
+export default connect(mapStateToProps)(SongDetail);
